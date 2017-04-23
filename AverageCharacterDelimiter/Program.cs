8a50aab057862d6c1077c136b2de6e5597babf28
ddunchev <danchodunchev@gmail.com>
2017-04-23 06:02:29 +0300

    Average character delimiter

---
+++ AverageCharacterDelimiter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageCharacterDelimiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader(@"F:\input.txt"));
+#endif
+
+            int sum = 0, index = 0;
+            string[] array = Console.ReadLine().Split().ToArray();
+
+            foreach (var item in array)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    sum += item[i];
+                    index++;
+                }
+            }
+
+            Console.WriteLine(String.Join($"{((char)(sum / index)).ToString().ToUpper()}", array));
+        }
+    }
+}
