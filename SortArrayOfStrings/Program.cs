2fb6dfdd9aa1a1b5cf62cc94680ea83240464d5f
ddunchev <danchodunchev@gmail.com>
2017-04-23 06:05:25 +0300

    Sort array of strings

---
+++ SortArrayOfStrings/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArrayUsingBubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader(@"F:\input.txt"));
+#endif
+
+
+            string[] myArray = Console.ReadLine().Split().ToArray();
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+
+                for (int i = 0; i < myArray.Length - 1; i++)
+                {
+                    if (String.Compare(myArray[i], myArray[i + 1]) > 0)
+                    {
+                        Swap(myArray, i, i + 1);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+            Console.WriteLine(String.Join(" ", myArray));
+        }
+
+        private static void Swap(string[] array, int i, int j)
+        {
+            string temp;
+            temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+    }
+}
